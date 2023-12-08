2d477c9af110a4ba0a3662e638019f32f027f99d
Jacaba2 <112039895+Jacaba2@users.noreply.github.com>
2023-12-08 18:38:21 -0500

    Update EthPriceOracle.sol

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -4,38 +4,47 @@ import "./Roles.sol";
 import "./SafeMath.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle {
+
   using Roles for Roles.Role;
   Roles.Role private owners;
   Roles.Role private oracles;
+
   using SafeMath for uint256;
+
   uint private randNonce = 0;
   uint private modulus = 1000;
-  uint private numOracles = 0;
-  uint private THRESHOLD = 0;
+  uint private numOracles = 1;
+  uint private THRESHOLD = 1;
+
   mapping(uint256=>bool) pendingRequests;
+
   struct Response {
     address oracleAddress;
     address callerAddress;
     uint256 ethPrice;
   }
   mapping (uint256=>Response[]) public requestIdToResponse;
+
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   event AddOracleEvent(address oracleAddress);
   event RemoveOracleEvent(address oracleAddress);
   event SetThresholdEvent (uint threshold);
   
-  constructor (address _owner) public {
+  //establishes owner of contract when deployed from 2_eth_price_oracle.js
+  constructor (address _owner, address _oracle) {
     owners.add(_owner);
+    oracles.add(_oracle);
   }
 
   function addOracle(address _oracle) public {
-    require(owners.has(msg.sender), "Not an owner!");
-    require(!oracles.has(_oracle), "Already an oracle!");
-    oracles.add(_oracle);
+    require(owners.has(msg.sender), "Not an owner!"); //veifying that msg.sender is an owner
+    require(!oracles.has(_oracle), "Already an oracle!"); //verifying tha the oracle address has not already been added as an oracle
+    oracles.add(_oracle); //adding oracle
     numOracles++;
     emit AddOracleEvent(_oracle);
   }
+
   function removeOracle (address _oracle) public {
     require(owners.has(msg.sender), "Not an owner!");
     require(oracles.has(_oracle), "Not an oracle!");
@@ -45,12 +54,15 @@ contract EthPriceOracle {
     emit RemoveOracleEvent(_oracle);
   }
 
+  //Setting threshold for #of responses needed to calculate Eth Price
   function setThreshold (uint _threshold) public {
     require(owners.has(msg.sender), "Not an owner!");
+    require(_threshold > 0, "Threshold cannot be 0");
     THRESHOLD = _threshold;
     emit SetThresholdEvent(THRESHOLD);
   }
 
+  //is called from EthPriceOracleInterface. Returns id which is the request id. Emits event to JS to fetch the data.
   function getLatestEthPrice() public returns (uint256) {
     randNonce++;
     uint id = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % modulus;
@@ -59,14 +71,16 @@ contract EthPriceOracle {
     return id;
   }
   
+  //Takes price of ether returned form client.js and 
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public {
-    require(oracles.has(msg.sender), "Not an oracle!");
-    require(pendingRequests[_id], "This request is not in my pending list.");
-    Response memory resp;
-    resp = Response(msg.sender, _callerAddress, _ethPrice);
-    requestIdToResponse[_id].push(resp);
-    uint numResponses = requestIdToResponse[_id].length;
-    if (numResponses == THRESHOLD) {
+    require(oracles.has(msg.sender), "Not an oracle!"); //Verifies that oracle is registered
+    require(pendingRequests[_id], "This request is not in my pending list."); // verifies that pending request is true
+    Response memory resp; // creates instance of response called resp
+    resp = Response(msg.sender, _callerAddress, _ethPrice); // sets values to the following
+    requestIdToResponse[_id].push(resp); // pushes values of resp to requestIdToresponse. 
+    uint numResponses = requestIdToResponse[_id].length; //finds # of reponses 
+    
+    if (numResponses == THRESHOLD) { // processes and updates the mean Eth price once a threshold of oracle responses have been completed.
       uint computedEthPrice = 0;
         for (uint f=0; f < requestIdToResponse[_id].length; f++) {
         computedEthPrice = computedEthPrice.add(requestIdToResponse[_id][f].ethPrice);
