9b8eeb35a798af54808b4ecd19adfbb9409658ef
Jacaba2 <112039895+Jacaba2@users.noreply.github.com>
2023-12-08 18:39:56 -0500

    Update Client.js

--- Client.js
+++ Client.js
@@ -1,55 +1,56 @@
 const common = require('./utils/common.js')
+require('./EthPriceOracle')
 const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 2000
 const PRIVATE_KEY_FILE_NAME = process.env.PRIVATE_KEY_FILE || './caller/caller_private_key'
 const CallerJSON = require('./caller/build/contracts/CallerContract.json')
 const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json')
+const {Web3} = require('web3')
+const ganacheUrl = 'ws://localhost:7545';
+const wsProvider = new Web3.providers.WebsocketProvider(ganacheUrl);
+const web3js = new Web3(wsProvider);
+require("dotenv").config();
 
-async function getCallerContract (web3js) {
-  const networkId = await web3js.eth.net.getId()
+//retrieves info for caller contract
+async function getCallerContract (web3js, networkId) {
   return new web3js.eth.Contract(CallerJSON.abi, CallerJSON.networks[networkId].address)
 }
 
-async function retrieveLatestEthPrice () {
-  const options = {
-    headers: {
-      'x-access-token': 'coinranking84143d427d79aedad184663a6001b2a1369076ed43a01a3f',
-    },
-  };
-  
-  fetch('https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/price', options)
-    .then((response) => response.json())
-    .then((result) => console.log(result.data.price));
-  return result.data.price
-}
-
+//Watches for events fired from oracle contract and carries out tasks based on the events
 async function filterEvents (callerContract) {
-  callerContract.events.PriceUpdatedEvent({ filter: { } }, async (err, event) => {
-    if (err) console.error('Error on event', err)
-    console.log('* New PriceUpdated event. ethPrice: ' + event.returnValues.ethPrice)
-  })
+  
+  callerContract.getPastEvents('PriceUpdatedEvent', {
+    fromBlock: 'latest',
+    toBlock: 'latest'
+  }).then(
+    function(events){
+      events.forEach(event => {
+        console.log('* New PriceUpdated event. ethPrice: ' + event.returnValues.ethPrice)
+      })
+    }
+  )
   callerContract.events.ReceivedNewRequestIdEvent({ filter: { } }, async (err, event) => {
     if (err) console.error('Error on event', err)
   })
 }
 
+//initalized vairables of main function below
 async function init () {
-  const { ownerAddress, web3js, client } = common.loadAccount(PRIVATE_KEY_FILE_NAME)
-  const callerContract = await getCallerContract(web3js)
-  filterEvents(callerContract)
-  return { callerContract, ownerAddress, client, web3js }
+  const networkId = await web3js.eth.net.getId()
+  const ownerAddress = process.env.OWNER_ADDRESS
+  const callerContract = await getCallerContract(web3js, networkId)
+  return { callerContract, ownerAddress, web3js, networkId }
 }
 
+//main function
 (async () => {
-  const { callerContract, ownerAddress, client, web3js } = await init()
-  process.on( 'SIGINT', () => {
-    console.log('Calling client.disconnect()')
-    client.disconnect();
-    process.exit( );
-  })
-  const networkId = await web3js.eth.net.getId()
-  const oracleAddress =  OracleJSON.networks[networkId].address
+  const { callerContract, ownerAddress, web3js, networkId} = await init()
+
+  const oracleAddress = OracleJSON.networks[networkId].address
   await callerContract.methods.setOracleInstanceAddress(oracleAddress).send({ from: ownerAddress })
+  
   setInterval( async () => {
-    await callerContract.methods.updateEthPrice().send({ from: ownerAddress })
+    await filterEvents(callerContract)
+    await callerContract.methods.updateEthPrice().send({ from: ownerAddress, gas: '1000000' })
   }, SLEEP_INTERVAL);
+
 })()
