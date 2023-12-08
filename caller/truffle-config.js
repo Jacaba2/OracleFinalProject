8bc60fe85e049b03bf850c73360d47e0c75e4cd1
Jacaba2 <112039895+Jacaba2@users.noreply.github.com>
2023-12-08 18:36:32 -0500

    Update truffle-config.js

--- caller/truffle-config.js
+++ caller/truffle-config.js
@@ -1,25 +1,29 @@
-const LoomTruffleProvider = require('loom-truffle-provider')
-
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require('fs');
 const path = require('path')
-const fs = require('fs')
+const { join } = require('path')
 
 module.exports = {
   networks: {
     developement:{
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*",
     },
-//    //extdev: {
- //     provider: function () {
-///        const privateKey = fs.readFileSync(path.join(__dirname, 'caller_private_key'), 'utf-8')
- //       const chainId = 'extdev-plasma-us1'
-//        const writeUrl = 'wss://extdev-plasma-us1.dappchains.com/websocket'
-//        const readUrl = 'wss://extdev-plasma-us1.dappchains.com/queryws'
-   //     return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
-///      },
-     // network_id: '9545242630824'
-   // }
+    sepolia: {
+      provider: () => new HDWalletProvider({
+      mnemonic: {
+      phrase: "spice then february index pelican mosquito cruel swift energy addict truly notable"
+      },
+      providerOrUrl: "wss://sepolia.infura.io/ws/v3/692bfe2cc11f46cf8dd2847cd8c74910"
+      }),
+      network_id: 11155111, // Sepolia's network ID
+      gas: 4000000, // Adjust the gas limit as per your requirements
+      gasPrice: 10000000000, // Set the gas price to an appropriate value
+      confirmations: 2, // Set the number of confirmations needed for a transaction
+      timeoutBlocks: 200, // Set the timeout for transactions
+      skipDryRun: true // Skip the dry run option
+     }
   },
   compilers: {
     solc: {
