9513c7e20902f13aff1aa700dd9bf0678cc392df
Jacaba2 <112039895+Jacaba2@users.noreply.github.com>
2023-12-09 13:21:45 -0500

    Update EthPriceOracle.js

--- EthPriceOracle.js
+++ EthPriceOracle.js
@@ -1,5 +1,5 @@
 const BN = require('bn.js') //Big number library
-const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 10000
+const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 2000
 const PRIVATE_KEY_FILE_NAME = process.env.PRIVATE_KEY_FILE || './oracle/oracle_private_key' // private key location
 const CHUNK_SIZE = process.env.CHUNK_SIZE || 3 
 const MAX_RETRIES = process.env.MAX_RETRIES || 5
